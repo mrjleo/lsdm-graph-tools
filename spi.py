52437154725b2fb5bc1e4a13c29a20f12b003eda
mrjleo <mr.ljleo@gmail.com>
2016-07-03 18:59:05 +0200

    add shortest path indexing boilerplate

---
+++ spi.py
@@ -0,0 +1,21 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+import argparse
+from graph import Graph
+
+
+def main():
+	ap = argparse.ArgumentParser()
+	ap.add_argument('INPUT_FILE')
+	args = ap.parse_args()
+
+	print('reading file \'{}\'...'.format(args.INPUT_FILE))
+	g = Graph.from_file(args.INPUT_FILE)
+	print('done!')
+	print(g.graph)
+
+
+if __name__ == '__main__':
+	main()
